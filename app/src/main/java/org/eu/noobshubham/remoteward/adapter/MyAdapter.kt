c9e783906cbe6e8f5436479466390a7f49f662bb
noobshubham <noobshubham65@gmail.com>
2023-10-21 14:00:18 +0530

    First Commit

---
+++ app/src/main/java/org/eu/noobshubham/remoteward/adapter/MyAdapter.kt
@@ -0,0 +1,60 @@
+package org.eu.noobshubham.remoteward.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import org.eu.noobshubham.remoteward.R
+import org.eu.noobshubham.remoteward.model.Todo
+
+
+class MyAdapter(private val todoList: ArrayList<Todo>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+    private var listener: OnClickListener? = null
+
+    fun setListener(clickListener: OnClickListener) {
+        this.listener = clickListener
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val todo: Todo = todoList[position]
+        holder.bindItems(todo)
+
+        holder.itemView.setOnClickListener {
+            if (listener != null) {
+                listener!!.onItemClick(todo, position)
+            }
+        }
+
+        holder.itemView.findViewById<ImageView>(R.id.btDelete).setOnClickListener {
+            if (listener != null) {
+                listener!!.onItemDelete(todo)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todoList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false))
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(todo: Todo) {
+            val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
+            val tvDesc = itemView.findViewById<TextView>(R.id.tvDesc)
+            tvTitle.text = todo.title
+            tvDesc.text = todo.desc
+        }
+    }
+
+    interface OnClickListener {
+        fun onItemClick(todo: Todo, position: Int)
+        fun onItemDelete(todo: Todo)
+    }
+}
